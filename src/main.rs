eae6606c7c5598a36c0c208c1fb5526e91c66188
NakagamiYuta <guguru0014@gmail.com>
2020-04-24 17:59:39 +0900

    [add] main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+extern crate regex_executor;
+use regex_executor::automaton::nfa::{ NFA, NFAError };
+
+fn main() -> Result<(), NFAError> {
+    // NFA構成
+    let chains = vec![
+        (0, 0, 'a'), (0, 0, 'b'), (0, 1, 'a'), (1, 1, 'a'), (1, 1, 'b'),
+        (1, 2, 'a'), (2, 2, 'a'), (2, 2, 'b'), (2, 3, 'b'), (2, 0, '@')
+    ];
+    let mut nfa = NFA::new(0, 3);
+    nfa.set_start(0)?;
+    nfa.set_finish(3)?;
+    for chain in &chains {
+        nfa.set_chain(chain.0, chain.1, chain.2)?;
+    }
+
+    // 実行
+    let target = String::from("abababbbabababbbaabbaab");
+    println!("pattern: (a|b)* aab");
+    println!("target str: \"{}\"", target);
+    println!("result: {}", nfa.simulate(target));
+    Ok(())
 }
